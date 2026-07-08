ba2a50ca49179f39353449904371b8076d4a7568
Sanju-Shrestha <sanjushresthanp@gmail.com>
2026-07-08 14:23:50 +0545

    Changes in settings.py 4

--- connecthub/settings.py
+++ connecthub/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG      = config("DEBUG", default=False, cast=bool)
 
 
-ALLOWED_HOSTS = config(
-    'ALLOWED_HOSTS',
-    default='localhost,127.0.0.1',
-).split(',')
+ALLOWED_HOSTS = 'connecthub-1-no8d.onrender.com'
 
 
 
