1341512bdf819b646d0ffd5ac8dbd2fe6ec877dd
Sanju-Shrestha <sanjushresthanp@gmail.com>
2026-07-08 14:19:32 +0545

    Changes in settings.py 3

--- connecthub/settings.py
+++ connecthub/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/" 
-STATICFILES_DIRS = [BASE_DIR / "staticfiles"] 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
     "staticfiles": {
